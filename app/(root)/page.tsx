23198fd761563c1caa49fe2f1a424f82837fea30
kemval <kemval@icloud.com>
2024-08-14 07:48:32 -0600

    implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,28 +1,35 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSideBar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 const Home = () => {
-    const loggedIn = {firstName: 'Hope'};
-    return(
-        <section className="home">
-            <div className="home-content">
-                <header className="home-header">
-                    <HeaderBox
-                        type = "greeting"
-                        title = "Welcome"
-                        user={loggedIn?.firstName || 'Guest'}
-                        subtext= "Access and manage your account and transactions efficiently."
-                    />
-                    <TotalBalanceBox
-                        accounts={[]}
-                        totalBanks={1}
-                        totalCurrentBalance={1250.35}
-                    />
-                </header>
-
-            </div>
-        </section>
-    )
-}
+  const loggedIn = { firstName: "Hope", lastName:' Shinning', email:'contact@gummietech.com' };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        recent transactions
+      </div>
+      <RightSideBar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 123.50},{currentBalance: 500.50}]}
+      
+      />
+    </section>
+  );
+};
 
 export default Home;
---
+++ app/(root)/transaction-history/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransactionHistory = () => {
+  return (
+    <div>
+        transactionHistory
+    </div>
+  )
+}
+
+export default TransactionHistory
